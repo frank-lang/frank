19baa2bdc712682cb27764a32f4a15d3d5257f45
Craig McLaughlin <mr_mcl@live.co.uk>
2016-08-30 12:30:44 +0100

    Added helper functions for updating components of the state.

--- RefineSyntax.hs
+++ RefineSyntax.hs
@@ -1,40 +1,86 @@
 module RefineSyntax where
 
+import Control.Monad
+import Control.Monad.Except
+import Control.Monad.State
+
 import Syntax
 
--- Some type synonyms for code commentary
-type EffectItfs = [RawTopTm]
-type DataTCol = [RawTopTm]
-type MHSigCol = [RawTopTm]
-type MHDefCol = [RawTopTm]
+type Refine = ExceptT String (State RState)
 
-type CtrCol = [Id]
-type CmdCol = [Id]
+data RState = MkRState { interfaces :: [Id]
+                       , datatypes :: [Id]
+                       , handlers :: [Id]
+                       , ctrs :: [Id]
+                       , cmds :: [Id]
+                       , program :: Prog}
 
-refine :: RawProg -> Prog
-refine prog = runRefinement prog
+getRState :: Refine RState
+getRState = get
 
-getItfs :: [RawTopTm] -> EffectItfs -> EffectItfs
+putRState :: RState -> Refine ()
+putRState = put
+
+putItfs :: [Id] -> Refine ()
+putItfs xs = do s <- getRState
+                putRState $ s { interfaces = xs }
+
+getItfs :: [RawTopTm] -> [Itf] -> [Itf]
 getItfs ((MkRawItfTm itf) : xs) ys = getItfs xs (itf : ys)
 getItfs (_ : xs) ys = getItfs xs ys
 getItfs [] ys = ys
 
-getDataTs :: [RawTopTm] -> DataTCol -> DataTCol
+collectINames :: [Itf] -> [Id]
+collectINames ((MkItf itf _ _) : xs) = itf : (collectINames xs)
+collectINames [] = []
+
+collectCmds :: [Cmd] -> [Id]
+collectCmds ((MkCmd cmd _) : xs) = cmd : (collectCmds xs)
+collectCmds [] = []
+
+getDataTs :: [RawTopTm] -> [DataT] -> [DataT]
 getDataTs ((MkRawDataTm dt) : xs) ys = getDataTs xs (dt : ys)
 getDataTs (_ : xs) ys = getDataTs xs ys
 getDataTs [] ys = ys
 
-getHdrSigs :: [RawTopTm] -> MHSigCol -> MHSigCol
+collectDTNames :: [DataT] -> [Id]
+collectDTNames ((MkDT dt _) : xs) = dt : (collectDTNames xs)
+collectDTNames [] = []
+
+collectCtrs :: [Ctr] -> [Id]
+collectCtrs ((MkCtr ctr _) : xs) = ctr : (collectCtrs xs)
+collectCtrs [] = []
+
+getHdrSigs :: [RawTopTm] -> [MHSig] -> [MHSig]
 getHdrSigs ((MkRawSigTm sig) : xs) ys = getHdrSigs xs (sig : ys)
 getHdrSigs (_ : xs) ys = getHdrSigs xs ys
 getHdrSigs [] ys = ys
 
-getHdrDefs :: [RawTopTm] -> MHDefCol -> MHDefCol
+getHdrDefs :: [RawTopTm] -> [MHCls] -> [MHCls]
 getHdrDefs ((MkRawDefTm def) : xs) ys = getHdrDefs xs (def : ys)
 getHdrDefs (_ : xs) ys = getHdrDefs xs ys
 getHdrDefs [] ys = ys
 
-refine' :: CtrNames -> CmdNames -> MHNames -> RawProg -> Prog
-refine' cs cmds mhs prog = MkProg
+-- Add the name if not already present
+addName :: [Id] -> Id -> Either String [Id]
+addName xs x = if x `elem` xs then Left (show x ++ " already defined in " ++
+                                         (show xs))
+               else Right (x : xs)
 
-refineClause :: RawClause -> Clause
+refine :: RawProg -> Either String Prog
+refine prog = evalState (runExceptT (refine' prog)) initRefine
+
+refine' :: RawProg -> Refine Prog
+refine' prog = do i <- getRState
+                  return $ program i
+
+{-- The initial state for the refinement pass. -}
+
+builtinCmds :: [Id]
+builtinCmds = ["putStrLn", "strcat"]
+
+builtinItfs :: [Id]
+builtinItfs = ["Console"]
+
+initRefine :: RState
+initRefine = MkRState builtinItfs [] [] [] builtinCmds (MkProg [])
