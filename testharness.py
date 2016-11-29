07cce0bd1e1fe75a8b75723522c73251c4f5b7be
Craig McLaughlin <mr_mcl@live.co.uk>
2016-11-29 17:47:45 +0000

    Added initial testing ideas and test harness (Python).

---
+++ testharness.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+## Test harness for Frank
+## Craig McLaughlin
+## See the musings document for details on the proposed specification for this
+## harness.
+
+def main(okDirs,errDirs):
+    print("I'm finished.")
+
+if __name__ == "__main__":
+    main(["tests/should-pass","tests/should-fail"])
