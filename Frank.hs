7b3bf6da16b05a818a1e294d2b249d45b78ab104
Craig McLaughlin <mr_mcl@live.co.uk>
2016-12-05 18:08:50 +0000

    Added helper library for parsing cmd line args. Refactored Main.

--- Frank.hs
+++ Frank.hs
@@ -10,44 +10,58 @@ import qualified ExpectedTestOutput as ETO
 
 import Shonky.Semantics
 
+import System.Console.CmdArgs.Explicit
 import System.Environment
 import System.Exit
 import System.IO
 
-parseAndCheckProg fileName b =
-  let progName = takeWhile (\c -> c /= '.') fileName in
-  do p <- runTokenParse <$> readFile fileName
-     case p of
-       Left err -> die err
-       Right prog -> case refine prog of
-         Left err -> die err
-         Right p' -> case check (desugar p') of
-          Left err -> die err
-          Right p' -> do env <- if b then
-                                  do compileToFile p' progName
-                                     loadFile progName
-                                else return $ load $ compile p'
-                         return env
+parseProg fileName _ = runTokenParse <$> readFile fileName
 
-compileAndRunProg progName b =
-  do env <- parseAndCheckProg progName b
-     case try env "main()" of
-       Ret v -> putStrLn $ ppVal v
-       comp -> do v <- ioHandler comp
-                  putStrLn $ ppVal v
+checkProg p _ =
+  case p of
+    Left err -> die err
+    Right p -> case refine p of
+      Left err -> die err
+      Right p' -> case check (desugar p') of
+        Left err -> die err
+        Right p' -> return p'
 
-run x = compileAndRunProg x False
+compileProg progName p args =
+  do env <- if ("output-shonky","") `elem` args then
+              do compileToFile p progName
+                 loadFile progName
+            else return $ load $ compile p
+     return env
 
-runWithOpts x opts =
-  case opts of
-    "--output-shonky" : opts -> compileAndRunProg x True
-    _ -> run x
+evalProg env tm =
+  case try env tm of
+    Ret v -> putStrLn $ ppVal v
+    comp -> do v <- ioHandler comp
+               putStrLn $ ppVal v
+
+compileAndRunProg fileName args =
+  do let progName = takeWhile (\c -> c /= '.') fileName
+     p <- parseProg fileName args
+     p' <- checkProg p args
+     env <- compileProg progName p' args
+     case lookup "eval" args of
+       Just v -> evalProg env v
+       Nothing -> evalProg env "main()"
+
+arguments :: Mode [(String,String)]
+arguments =
+  mode "frank" [] "Frank program" (flagArg (upd "file") "FILE")
+  [flagNone ["output-shonky"] (("output-shonky",""):) "Output Shonky code"
+  ,flagHelpSimple (("help",""):)]
+  where upd msg x v = Right $ (msg,x):v
 
 main :: IO ()
 main = do
   hSetBuffering stdin NoBuffering
   hSetEcho stdin False
-  args <- getArgs
-  case args of
-    file : opts -> runWithOpts file opts
-    _      -> run "tests/paper.fk"
+  args <- processArgs arguments
+  if ("help","") `elem` args then
+     print $ helpText [] HelpFormatDefault arguments
+  else case lookup "file" args of
+    Just f -> compileAndRunProg f args
+    Nothing -> die "no Frank source specified."
