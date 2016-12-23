3f8c8ee43e0cc76496a97fd21a879781c425502d
Craig McLaughlin <mr_mcl@live.co.uk>
2016-12-23 00:53:05 +0000

    Fix Issue #7: perform one unfolding of flex-evars in ALL cases.
    Previously, unfolding only occurred in the case of unifying two
    abilities which both contained a flexible ability/effect
    variable. Unfortunately, issue #7 shows this to be unsound. So
    unfolding is now performed in all cases.
    
    Moreover, it is performed on either ability (rather than both or
    none, as previously). Finally, this change also enforces that only
    one unfolding is attempted before yielding to solveForEVar. The
    previous version of unifyAb potentially did multiple unfoldings
    before yielding. It is unclear whether it matters but no regression
    tests have failed due to this change.

--- Unification.hs
+++ Unification.hs
@@ -5,6 +5,8 @@ module Unification where
 import qualified Data.Map as M
 import qualified Data.Set as S
 
+import Debug.Trace
+
 import Control.Monad.Except
 
 import BwdFwd
@@ -30,8 +32,10 @@ onTop f = popEntry >>= focus
                Restore -> modify (:< e)
         focus e = onTop f >> modify (:< e)
 
-findEVar :: Id -> Contextual (Maybe (Ab Desugared))
-findEVar x = getContext >>= find'
+findAbVar :: AbMod Desugared -> Contextual (Maybe (Ab Desugared))
+findAbVar MkEmpAb = return Nothing
+findAbVar (MkAbRVar _) = return Nothing
+findAbVar (MkAbFVar x) = getContext >>= find'
   where find' BEmp = return Nothing
         find' (es :< FlexMVar y (AbDefn ab)) | x == y = return $ Just ab
         find' (es :< _) = find' es
@@ -70,29 +74,45 @@ unify t            s                 =
   (show $ ppVType t) ++ " with " ++ (show $ ppVType s)
 
 unifyAb :: Ab Desugared -> Ab Desugared -> Contextual ()
-unifyAb (MkAb (MkAbFVar a0) m0) (MkAb (MkAbFVar a1) m1) =
-  do unifyItfMap (M.intersection m0 m1) (M.intersection m1 m0)
-     ma0 <- findEVar a0
-     ma1 <- findEVar a1
+unifyAb ab0@(MkAb v0 m0) ab1@(MkAb v1 m1) =
+  do ma0 <- findAbVar v0
+     ma1 <- findAbVar v1
      case (ma0, ma1) of
        (Just (MkAb v0 m0'), Just (MkAb v1 m1')) ->
          let m0'' = M.union m0 m0' in
          let m1'' = M.union m1 m1' in
-         unifyAb (MkAb v0 m0'') (MkAb v1 m1'')
-       (_, _) ->
-         do v <- MkAbFVar <$> freshMVar "£"
-            solveForEVar a0 [] (MkAb v (M.difference m1 m0))
-            solveForEVar a1 [] (MkAb v (M.difference m0 m1))
-unifyAb (MkAb (MkAbFVar a0) m0) (MkAb v m1) | M.null (M.difference m0 m1) =
-  do unifyItfMap (M.intersection m1 m0) (M.intersection m0 m1)
-     solveForEVar a0 [] (MkAb v (M.difference m1 m0))
-unifyAb (MkAb v m0) (MkAb (MkAbFVar a1) m1) | M.null (M.difference m1 m0) =
-  do unifyItfMap (M.intersection m0 m1) (M.intersection m1 m0)
-     solveForEVar a1 [] (MkAb v (M.difference m0 m1))
-unifyAb (MkAb v0 m0) (MkAb v1 m1) | v0 == v1 = unifyItfMap m0 m1
-unifyAb ab0 ab1 =
-  throwError $ "cannot unify abilities " ++ (show $ ppAb ab0) ++ " and " ++
-  (show $ ppAb ab1)
+         unifyAb' (MkAb v0 m0'') (MkAb v1 m1'')
+       (Just (MkAb v0 m0'), Nothing) ->
+         let m0'' = M.union m0 m0' in
+         unifyAb' (MkAb v0 m0'') ab1
+       (Nothing, Just (MkAb v1 m1')) ->
+         let m1'' = M.union m1 m1' in
+         unifyAb' ab0 (MkAb v1 m1'')
+       (Nothing, Nothing) ->
+         unifyAb' ab0 ab1
+  where unifyAb' (MkAb (MkAbFVar a0) m0) (MkAb (MkAbFVar a1) m1) =
+          do unifyItfMap (M.intersection m0 m1) (M.intersection m1 m0)
+             v <- MkAbFVar <$> freshMVar "£"
+             solveForEVar a0 [] (MkAb v (M.difference m1 m0))
+             solveForEVar a1 [] (MkAb v (M.difference m0 m1))
+        unifyAb' (MkAb (MkAbFVar a0) m0) (MkAb v m1)
+          | M.null (M.difference m0 m1) =
+            do unifyItfMap (M.intersection m1 m0) (M.intersection m0 m1)
+               solveForEVar a0 [] (MkAb v (M.difference m1 m0))
+        unifyAb' (MkAb v m0) (MkAb (MkAbFVar a1) m1)
+          | M.null (M.difference m1 m0) =
+            do unifyItfMap (M.intersection m0 m1) (M.intersection m1 m0)
+               solveForEVar a1 [] (MkAb v (M.difference m0 m1))
+        unifyAb' ab0@(MkAb v0 m0) ab1@(MkAb v1 m1) | v0 == v1 =
+          catchError (unifyItfMap m0 m1) (unifyAbError ab0 ab1)
+        unifyAb' ab0 ab1 =
+          throwError $ "cannot unify abilities " ++ (show $ ppAb ab0) ++
+          " and " ++ (show $ ppAb ab1)
+
+unifyAbError :: Ab Desugared -> Ab Desugared -> String -> Contextual ()
+unifyAbError ab0 ab1 _ =
+  throwError $ "cannot unify abilities " ++ (show $ ppAb ab0) ++
+  " and " ++ (show $ ppAb ab1)
 
 unifyItfMap :: ItfMap Desugared -> ItfMap Desugared -> Contextual ()
 unifyItfMap m0 m1 = do mapM_ (unifyItfMap' m1) (M.toList m0)
